d5a7c1b6c8c1d2cf14eadc052b2152f5c5dad456
Stephany <huescaascencio@gmail.com>
2026-05-16 21:30:37 +0000

    Cree el codigo foreach

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+class Program
+{
+    static void Main ()
+    {
+        int [] notas = {85,92,78,95,61};
+        foreach (int nota in notas)
+
+        Console.WriteLine(nota);
+    }
+}
